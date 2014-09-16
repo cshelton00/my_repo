8d41c04d739b0b18216ceb0defbb778a26fb6b3b
cshelton00 <chrisshelton2232@gmail.com>
2014-09-16 19:43:03 -0400

    added login menu and playing with box shadow

--- responsive page/js/js.js
+++ responsive page/js/js.js
@@ -5,3 +5,11 @@ $(this).children('.block_overlay').slideToggle(175);
 $('#hamburger').click(function(){
         $('.burger_nav').slideToggle(0);
 });
+
+  $('.toggle ').click(function() {
+      $('.login').toggleClass('open');
+    });
+
+$(".toggle").click(function() {
+  $('.arrow_down').toggleClass("arrow_up");
+});
